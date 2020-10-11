9ddf7af10d62be1919268687c68331c366925c0b
Jakob Steimle <jakob.steimle@tum.de>
2020-10-11 21:50:39 +0200

    digital ocean files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='id-pro-backend-deploy',
+    version='',
+    packages=['nanoid', 'twilio', 'flask', 'flask_cors'],
+    url='',
+    license='',
+    author='Jakob Steimle',
+    author_email='',
+    description=''
+)
